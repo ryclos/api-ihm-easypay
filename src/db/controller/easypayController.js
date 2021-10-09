f1ec7dcca1b0fe1d1f2ffda5f94a2ec612fc1310
hpambrun <hpambrun@WAMP31.OSB.PF>
2021-10-08 15:44:16 -1000

    Changement des requêtes et amélioration du model

--- src/db/controller/easypayController.js
+++ src/db/controller/easypayController.js
@@ -13,32 +13,30 @@ const getAllOsbCommercant = async (req, res) => {
     }
 };
 
-const addNewOsbCommercant = async (req, res) =>{
-    try {
-        const body = req.body;
-        const osbCommercant = await OsbCommercant.create(body);
-        res.status(201).json(osbCommercant);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-};
+//const addNewOsbCommercant = async (req, res) =>{
+//    try {
+//        const body = req.body;
+//        const osbCommercant = await OsbCommercant.create(body);
+//        res.status(201).json(osbCommercant);
+//    } catch (error) {
+//        res.status(500).json({error});
+//    }
+//};
 
-const deleteOsbCommercantById = async (req, res) => {
-    try {
-        const id = req.params.id
-        const osbCommercant = await OsbCommercant.findByPk(id)
-        const OsbCommercantDeleted = osbCommercant
-        const deleteOsbCommercant = (await osbCommercant).destroy({
-            where: {
-                id: OsbCommercantDeleted.id
-            }
-        })
-        const message = `Le commerçant avec l'identifiant n°${OsbCommercantDeleted.id} a bien été supprimé.`
-        res.json({message, data: OsbCommercantDeleted })
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+//const deleteOsbCommercantById = async (req, res) => {
+//    try {
+//        const id = req.params.id
+//        const osbCommercant = await OsbCommercant.findByPk(id)
+//        if (osbCommercant === null) {
+//            const message = `Le commerçant n°${id} n'existe pas. Veuillez mettre le bon Numéro Tahiti`
+//            return res.status(404).json({message})
+//        }
+//        await osbCommercant.destroy(id)
+//        res.status(204)
+//    } catch (error) {
+//        res.status(500).json(error)
+//    }
+//}
 
 const getOsbCommercantById = async (req, res) => {
     try {
@@ -58,8 +56,14 @@ const updateOsbCommercant = async (req, res) => {
     try {
         const id = req.params.id
         const osbCommercant = await OsbCommercant.findByPk(id); // Récupérer le osbCommercant avant de le modifier
-        const updateOsbCommercant = await osbCommercant.update(req.body); // Une fois récupéré on peut le modifier
-        res.status(203).json(updateOsbCommercant)
+        if (osbCommercant === null) {
+            const message = `Le Numero Tahiti ${id} demandé n'existe pas. Réessayer avec un autre.`
+            return res.status(404).json({message})
+        }
+        const { email, logo, idBoutiquePzn, certificat, libelleSMS } = req.body // On récupère les champs de la table qu'on veut modifier
+        const updateChamps = { email, logo, idBoutiquePzn, certificat, libelleSMS } // Déclaration des objets sur les champs de la table
+        const updateOsbCommercant = await osbCommercant.update(updateChamps); // Une fois récupéré on peut le modifier
+        res.status(200).json(updateOsbCommercant)
     } catch (error) {
         res.status(500).json(error);
     }
@@ -67,8 +71,8 @@ const updateOsbCommercant = async (req, res) => {
 
 module.exports = {
     getAllOsbCommercant,
-    addNewOsbCommercant,
+    //addNewOsbCommercant,
     getOsbCommercantById,
     updateOsbCommercant,
-    deleteOsbCommercantById
+    //deleteOsbCommercantById
 };
--- src/routes/easypayRoute.js
+++ src/routes/easypayRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controller = require('../db/controller/easypayController');
 
 router.get('/', controller.getAllOsbCommercant);
-router.post('/', controller.addNewOsbCommercant);
+//router.post('/', controller.addNewOsbCommercant);
 router.get('/:id', controller.getOsbCommercantById);
 router.put('/:id', controller.updateOsbCommercant);
-router.delete('/:id', controller.deleteOsbCommercantById)
+//router.delete('/:id', controller.deleteOsbCommercantById)
 
 module.exports = router;
